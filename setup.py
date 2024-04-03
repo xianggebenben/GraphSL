55868b429e25d02448830d5a4c3e12d7a09da835
Junxiang Wang <xianggebenben@163.com>
2024-04-03 19:45:22 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,8 +9,7 @@ setup(
   author_email = 'junxiang.wang@alumni.emory.edu',      # Type in your E-Mail
   url = 'https://xianggebenben.github.io/Junxiang_Wang.github.io/',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/xianggebenben/GraphSL/archive/refs/tags/v_01.tar.gz',    # I explain this later on
-  keywords = ['Graph Diffusion', 'Graph Source Localization', 'Prescribed Methods', 'GNN Methods',
-'Benchmark'],   # Keywords that define your package best
+  keywords = ['Graph Diffusion', 'Graph Source Localization', 'Prescribed Methods', 'GNN Methods','Benchmark'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
           'networkx',
@@ -23,14 +22,13 @@ setup(
           'torch_geometric',
           'warnings',
           'operator',
-    'enum',
-'typing',
-'sys'
-
+          'enum',
+          'typing',
+          'sys'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Researchers',      # Define that your audience are developers
+    'Intended Audience :: Researchers',      # Define that your audience are researchers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
