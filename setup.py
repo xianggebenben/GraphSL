5ba2050ae96dc994763337e9cff43f6a7be4b381
Junxiang Wang <xianggebenben@163.com>
2024-04-03 22:47:24 -0400

    pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from distutils.core import setup
+from setuptools import find_packages
+
 setup(
   name = 'GraphSL',         # How you named your package folder (MyLib)
-  packages = ['GraphSL'],   # Chose the same as "name"
+  packages = find_packages('GraphSL',exclude=['all']),   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Graph Source Localization Approaches and Benchmark Datasets',   # Give a short description about your library
@@ -28,8 +30,6 @@ setup(
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Researchers',      # Define that your audience are researchers
-    'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
     'Programming Language :: Python :: 3.10'
