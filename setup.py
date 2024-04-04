91b8122cec429820859abbdb1dd16e600d4ed875
Junxiang Wang <xianggebenben@163.com>
2024-04-04 00:49:23 -0400

    update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 setup(
   name = 'GraphSL',         # How you named your package folder (MyLib)
   packages = find_packages('GraphSL',exclude=['all']),   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.1.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Graph Source Localization Approaches and Benchmark Datasets',   # Give a short description about your library
   author = 'Junxiang Wang',                   # Type in your name
@@ -15,7 +15,6 @@ setup(
   install_requires=[            # I get to this in a second
           'numpy',
           'networkx',
-          'random',
           'ndlib',
           'torch',
           'copy',
